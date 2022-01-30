7b55a22097a7e7c694bbc51355594d7de49ae427
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-01-30 19:58:06 +0200

    Add chanhge

--- src/NewExpense/ExpenseForm.js
+++ src/NewExpense/ExpenseForm.js
@@ -64,7 +64,7 @@ const ExpenseForm = () => {
         </div>
       </div>
       <div className='new-expense__actions'>
-        <button type='submit'>Add expense</button> 
+        <button type='submit'>Add nexpense</button> 
       </div>
     </form>
   )
