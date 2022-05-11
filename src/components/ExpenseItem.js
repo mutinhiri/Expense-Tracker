a8ac490edf65b76f7b228f22e63403330b9ed6a8
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 23:05:19 +0200

    Add year constant

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,6 +3,7 @@ import './ExpenseItem.css'
 function ExpenseItem(props) {
     const month = props.date.toLocaleString('en-US', { month: 'long'})
     const day = props.date.toLocaleString('en-US', {day: '2-digit'})
+    const year = props.date.toLocaleString('en-US', )
     return (
         <div className='expense-item'>
             <div>
