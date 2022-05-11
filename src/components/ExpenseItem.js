17372510fb95453f535950c415cfb6b60fdee1b7
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 20:41:54 +0200

    Add props data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -9,7 +9,7 @@ function ExpenseItem(props) {
             <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
                 <h2>{props.title }</h2>
-                <div className='expense-item__price'>{expenseAmount}</div>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>
     )
