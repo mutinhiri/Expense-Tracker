0d4bfa87d88d9dee612783d8e9ca90e4905e905c
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 22:59:37 +0200

    Fix bug

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,7 +7,7 @@ function ExpenseItem(props) {
     return (
         <div className='expense-item'>
             <div>
-                <div>{props.date.toLocalString('en-US', { month: 'long'})}</div>
+                <div>{props.date.toLocaleString('en-US', { month: 'long'})}</div>
                 <div>Year</div>
                 <div>Day</div>
             </div>
