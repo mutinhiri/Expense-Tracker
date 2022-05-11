ff21b1d10d945de835eb4b9a99030cac25cbd794
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 23:18:09 +0200

    Add new component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,9 +6,7 @@ function ExpenseItem(props) {
     return (
         <div className='expense-item'>
             <div>
-                <div>{ month }</div>
-                <div>{year}</div>
-                <div>{day}</div>
+                <ExpenseDate/>
             </div>
             <div className='expense-item__description'>
                 <h2>{props.title }</h2>
