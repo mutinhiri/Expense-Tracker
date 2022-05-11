f724878e41d2c4e0e7b09d12c16986324c0979f2
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 20:16:16 +0200

    Add date attribute

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expenses = [
     { title: "Car Insurance", amount: 294, date: new Date(2022, 3, 12)},
-    { title: "Medical expense", amount: 40, date: new Date(2022, ) }
+    { title: "Medical expense", amount: 40, date: new Date(2022, 3, 24) }
   ];
   return (
     <div>
