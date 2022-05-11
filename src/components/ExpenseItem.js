65fbcb440ab6bcb7bdb5c63c14304aaf6effdba3
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 15:35:02 +0200

    Add render files changes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,8 +2,9 @@ function ExpenseItem() {
     return (
         <div>
             <div>
-                <div><h2>Date</h2><div>amount</div></div>
-            </div>
+                <h2>Date</h2>
+                <div>amount</div>
+                </div>
         </div>
     )
 }
