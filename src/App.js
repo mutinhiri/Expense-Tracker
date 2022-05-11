f90263678249a78b9c9483c8c6ed939168aeec46
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 20:21:00 +0200

    Add new changes

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ function App() {
        amount: 294, 
        date: new Date(2022, 3, 12)
     },
+
     { 
       id: "e2",
       title: "Medical expense", 
       amount: 40, 
       date: new Date(2022, 3, 24)
     },
+    
     { title: "Food", amount: 60, date: new Date(2022, 4, 12)}
   ];
   return (
