b32f66b67f56fa1ef034a9b9fa26f70bfa35ba03
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 15:47:03 +0200

    Add expende item desc

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,7 +4,7 @@ function ExpenseItem() {
     return (
         <div className='expense-item'>
             <div>March 23 2022</div>
-            <div>
+            <div className='expense-item__description'>
                 <h2>Car Insurance</h2>
                 <div>$ 456</div>
             </div>
