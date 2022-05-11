51df7277948bca8fc1c993e8c8e4be7128a431d4
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 23:01:53 +0200

    Add local file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,7 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-    const expenseDate = new Date(2022, 2, 23);
-    const expenseTitle = 'Car Insurance'
-    const expenseAmount = 300
+    const month = props.date.toLocaleString('en-US', { month: 'long'})
     return (
         <div className='expense-item'>
             <div>
