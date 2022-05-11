9cae85e3a0c3f381faa1bbcfa291db021387b949
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 15:47:40 +0200

    Add pricing class

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,7 +6,7 @@ function ExpenseItem() {
             <div>March 23 2022</div>
             <div className='expense-item__description'>
                 <h2>Car Insurance</h2>
-                <div>$ 456</div>
+                <div className='expense-item__price'>$ 456</div>
             </div>
         </div>
     )
