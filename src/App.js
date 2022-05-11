254309d4f2b820d9b1f43692e0c31052a01faa84
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 20:13:52 +0200

    Add new changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const expenses = [
-    { title: "Car Insurance", amount: 294}
+    { title: "Car Insurance", amount: 294, date: new Date(2021, 3, 12)}
   ];
   return (
     <div>
