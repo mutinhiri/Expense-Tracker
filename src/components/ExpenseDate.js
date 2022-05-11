3462499007bcf16bf33e0303d45b6feed01b88c4
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-12 00:17:33 +0200

    Add month param

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -2,6 +2,10 @@ function ExpenseDate(props) {
     const month = props.date.toLocaleString('en-US', { month: 'long'})
     const day = props.date.toLocaleString('en-US', {day: '2-digit'})
     const year = props.date.getFullYear()
+
+    return (
+        <div>{month}</div>
+    )
 }
 
 export default ExpenseDate
