e3b7a3059c91962c57eefc4389c406747e4b24b6
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 20:16:36 +0200

    Add new object params

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expenses = [
     { title: "Car Insurance", amount: 294, date: new Date(2022, 3, 12)},
-    { title: "Medical expense", amount: 40, date: new Date(2022, 3, 24) }
+    { title: "Medical expense", amount: 40, date: new Date(2022, 3, 24) },
+    {}
   ];
   return (
     <div>
