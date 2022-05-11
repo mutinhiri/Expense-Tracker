c84d6499ab27ddbc9958a202a267776c03cc5751
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 19:33:38 +0200

    Add amount var

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,7 +2,8 @@ import './ExpenseItem.css'
 
 function ExpenseItem() {
     const expenseDate = new Date(2022, 2, 23);
-    const expenseTitle = 'Car Insurance '
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 300
     return (
         <div className='expense-item'>
             <div>March 23 2022</div>
