ff67d4c65e4f1455cdf85d19f894aa17ddb3adf0
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-11 20:32:39 +0200

    Add item expense

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
       <p>This is also visible</p>
       <ExpenseItem title= { expenses[0].title} amount= {expenses[0].amount} date={expenses[0].date}/>
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date = {expenses[1].date}/>
-      <ExpenseItem title={expenses[2].title} />
+      <ExpenseItem title={expenses[2].title} amount={expenses[2]} />
     </div>
   );
 }
