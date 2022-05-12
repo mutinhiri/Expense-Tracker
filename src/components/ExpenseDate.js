c67b41c2521090ca53aac3a966bfa064cdd10e0e
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-12 20:28:09 +0200

    Add display render

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -8,7 +8,7 @@ function ExpenseDate(props) {
     return (
         <div className='expense-date'>
             <div className="expense-date__month">{ month }</div>
-            <div>{ year }</div>
+            <div className='expense-date__year'>{ year }</div>
             <div>{ day }</div>
         </div>
     )
