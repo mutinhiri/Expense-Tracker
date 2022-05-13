97f06837523e31d9d8816db997fbf31a069cf565
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-13 21:32:00 +0200

    Add changes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -12,7 +12,7 @@ function ExpenseItem(props) {
         <div className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title }</h2>
+                <h2>{title }</h2>
                 <div className='expense-item__price'>{props.amount}</div>
             </div>
             <button onClick={() => {console.log('licked ')}}>Change title</button>
