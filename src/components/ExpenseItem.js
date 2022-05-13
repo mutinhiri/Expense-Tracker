a7f8f2e5481199d86216a7066678beeb0e52e761
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-13 21:33:11 +0200

    call app

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -15,7 +15,7 @@ function ExpenseItem(props) {
                 <h2>{title }</h2>
                 <div className='expense-item__price'>{props.amount}</div>
             </div>
-            <button onClick={}>Change title</button>
+            <button onClick={clickHandler}>Change title</button>
         </div>
     )
 }
