1a30f327b286e9e1724b5838598da8a198bd06b7
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-13 18:22:17 +0200

    Dynamic class

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import './Card.css'
 
 function Card(props) {
     const classes = 'card' + props.className
-    return <div className="card">{props.children}</div>
+    return <div className={classes}>{props.children}</div>
 }
 
 
