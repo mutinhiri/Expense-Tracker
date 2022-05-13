8925adc6ea305e1c38655d691f29745ae923d1d7
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-13 21:16:42 +0200

    Add new button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,13 +5,14 @@ import './ExpenseItem.css'
 function ExpenseItem(props) {
 
     return (
-        <Card className='expense-item'>
+        <div className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title }</h2>
                 <div className='expense-item__price'>{props.amount}</div>
             </div>
-        </Card>
+            <button>Change title</button>
+        </div>
     )
 }
 
