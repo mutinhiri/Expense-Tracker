a9be2668cb30706c66d4943f4108cce09348fdeb
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-14 15:39:38 +0200

    Add file render

--- src/NewExpense/ExpenseForm.js
+++ src/NewExpense/ExpenseForm.js
@@ -5,7 +5,9 @@ const ExpenseForm = () => {
   return(
     <form>
       <div className='new-expense__controls'>
-        
+        <div className='new-expense'>
+
+        </div>
       </div>
     </form>
   )
