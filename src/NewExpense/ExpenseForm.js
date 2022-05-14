c2b59780c30e7d2d65b7448b9adc0b3ffdbb1692
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-14 15:54:56 +0200

    Add button

--- src/NewExpense/ExpenseForm.js
+++ src/NewExpense/ExpenseForm.js
@@ -21,7 +21,7 @@ const ExpenseForm = () => {
         </div>
       </div>
       <div className='new-expense__actions'>
-        <button>Add expense</button>
+        <button type='submit'>Add expense</button>
       </div>
     </form>
   )
