00ad404a89ad42c698db83ff4974aa462bcad5a1
mutinhiri <barnabmutinhiri@gmail.com>
2022-05-14 20:18:30 +0200

    Add para

--- src/NewExpense/ExpenseForm.js
+++ src/NewExpense/ExpenseForm.js
@@ -47,7 +47,7 @@ const ExpenseForm = () => {
           <label>Title</label>
           <input type="text" onChange={ titleChangeHandler }/>
         </div>
-        <p>{new Date(2022-02-10)}</p>
+        <p>{() => { new Date('2022-03-02')}}</p>
         
         <div className='new-expense__control'>
           <label>Amount</label>
