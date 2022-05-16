5debd52d493db25b4b48ae7a7d193468600e8db0
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-16 18:54:11 +0200

    Add new properties

--- src/NewExpense/NewExpense.js
+++ src/NewExpense/NewExpense.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 
-const NewExpense = () => {
+const NewExpense = (props) => {
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
