61449206fe8bbae8f34643fb6d6eb80d5ae1d85e
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-16 16:40:39 +0200

    Add new function to be called

--- src/NewExpense/NewExpense.js
+++ src/NewExpense/NewExpense.js
@@ -3,6 +3,9 @@ import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 
 const NewExpense = () => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        
+    }
     return(
         <div className='new-expense'>
             <ExpenseForm onSaveExpenseData/>
