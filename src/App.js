f350d7e2d896fed0dcf68d4c02f53b6e8e8d0629
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-19 18:13:30 +0200

    Add items

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
 
   return (
     <div>
-      {expenses.length === 0}
+      {expenses.length === 0 ? <p>No expenses found </p>}
       {expenses.map((expense, index) => (<ExpenseItem 
         key={expense.id}
         title={expense.title} 
