490b3155bd897d9885b95643aac4c3cbd04ca73b
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-20 22:31:02 +0200

    Add structure changes

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -6,9 +6,8 @@ const ChartBar = () => {
       <div className='chart-bar__inner'>
         <div className='chart-bar__fill'>
         </div>
-        <div className='chart-bar__label'></div>
-
       </div>
+      <div className='chart-bar__label'></div>
     </div>
   )
 }
