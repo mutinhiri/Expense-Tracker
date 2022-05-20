b4c71e3cd1d8bc9e1d46ed2dd6edb05244596f45
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-21 00:02:59 +0200

    Return new datapoint

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,7 +3,7 @@ import ChartBar from './ChartBar'
 import './Chart.css'
 
 const Chart = (props) => {
-  const datapointValues = props.dataPoints.map()
+  const datapointValues = props.dataPoints.map(dataPoint => dataPoint.value );
   const totalMaximum = Math.max();
   return (
     <div className='chart'>
