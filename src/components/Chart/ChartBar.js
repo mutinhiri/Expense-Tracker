11bd3a78e00e7f2d3bc6546addf75313ee76f709
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-20 22:25:57 +0200

    Add new chart bar

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ChartBar = () => {
   return (
     <div className='chart-bar'>
-      <div className=''>
+      <div className='chart-bar__inner'>
 
       </div>
     </div>
