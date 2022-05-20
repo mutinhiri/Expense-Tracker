1a336db1de2ee7a0673e2319b1de8b464ab564e2
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-21 00:01:26 +0200

    Add array map for data

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,6 +3,7 @@ import ChartBar from './ChartBar'
 import './Chart.css'
 
 const Chart = (props) => {
+  const datapointValues = props.dataPoints.map()
   return (
     <div className='chart'>
       {props.dataPoints.map((dataPoint) =>
