b0a8e0a793153ba3122e2474bb21f16fd0c7a005
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-20 23:40:31 +0200

    Add changes

--- src/components/Chart/ExpensesChart.js
+++ src/components/Chart/ExpensesChart.js
@@ -3,7 +3,8 @@ import Chart from './Chart'
 
 const ExpensesChart = (props) => {
   const chartDataPoints = [
-    {label: 'Jan', value: 0}
+    {label: 'Jan', value: 0},
+    {}
   ]
   return (
     <Chart />
