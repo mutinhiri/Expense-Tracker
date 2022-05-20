f5f5c47a8a07f26790a2ea1aa5b7d31d1b35aa3a
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-20 12:19:53 +0200

    Add signal for data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,36 @@ import React, { useState } from 'react'
 import ExpenseItem from "./components/ExpenseItem";
 import NewExpense from "./NewExpense/NewExpense";
 
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+     title: "Car Insurance",
+     amount: 294, 
+     date: new Date(2022, 3, 12)
+  },
+
+  { 
+    id: "e2",
+    title: "Medical expense", 
+    amount: 40, 
+    date: new Date(2022, 3, 24)
+  },
+
+  { 
+    id: "e3",
+    title: "Food", 
+    amount: 60, 
+    date: new Date(2022, 4, 12)
+  },
+
+  {
+    id: 'e5',
+    title: 'Hard times',
+    amount: 50,
+    date: new Date(2021, 8, 5)
+  }
+];
+
 function App() {
   const [expenses, setExpenses] = useState(INITIAL_EXPENSES)
 
