824fd683249770d25e2954e81b4281fc5c85620f
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-20 23:40:57 +0200

    Add february data

--- src/components/Chart/ExpensesChart.js
+++ src/components/Chart/ExpensesChart.js
@@ -4,7 +4,7 @@ import Chart from './Chart'
 const ExpensesChart = (props) => {
   const chartDataPoints = [
     {label: 'Jan', value: 0},
-    {}
+    {label: "Feb", value: 0}
   ]
   return (
     <Chart />
