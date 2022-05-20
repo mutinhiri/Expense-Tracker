4944c65c6a6f2de13b657c8b391ab870910e7136
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-20 13:43:39 +0200

    Add items length

--- src/components/ExpensesList.js
+++ src/components/ExpensesList.js
@@ -3,7 +3,7 @@ import React from 'react'
 import ExpenseItem from './ExpenseItem'
 
 const ExpensesList = (props) => {
-  {expenses.length === 0 ? <p>No expenses found </p>: expenses.map((expense, index) => (<ExpenseItem 
+  {props.item.length === 0 ? <p>No expenses found </p>: expenses.map((expense, index) => (<ExpenseItem 
     key={expense.id}
     title={expense.title} 
     amount={expense.amount} 
