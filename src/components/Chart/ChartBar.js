5081df0445b968ef6d5ca97108eded4fbbe297d4
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-20 22:29:23 +0200

    Add label class

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -4,9 +4,9 @@ const ChartBar = () => {
   return (
     <div className='chart-bar'>
       <div className='chart-bar__inner'>
-        <div className='chart-bar'>
-
+        <div className='chart-bar__fill'>
         </div>
+        <div className='chart-bar__label'></div>
 
       </div>
     </div>
